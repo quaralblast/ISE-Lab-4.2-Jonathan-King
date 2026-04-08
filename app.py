57d6d9fb068a4a92e5bd9c79f0a79305b32faa33
Jonathan King <jonathan.king1@bison.howard.edu>
2026-04-08 06:38:20 +0000

    Change CSS and HTML to improve UI

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flaskr import create_app
 
 app = create_app()
 
+# https://8080-cs-73483424128-default.cs-us-east1-vpcf.cloudshell.dev/
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
     app.run(debug=True, port=server_port, host='0.0.0.0')
